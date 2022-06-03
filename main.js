68b70487fa56676e683397915c56739529a292f0
David Gonzalez <Davidgonzalez_1999@yahoo.com>
2022-06-03 21:33:41 +0000

    Added a bit more context

--- main.js
+++ main.js
@@ -1,11 +1,21 @@
 var modalButton = document.querySelector('.modal-btn');
 modalButton.addEventListener('click', handleClick);
 var form = document.querySelector('form');
+var spans = document.querySelectorAll('#week');
+spans.addEventListener('click', handleClickedButton);
 function handleClick(event) {
   if (event.target === modalButton) {
     form.classList.remove('hidden');
 
   } else {
     form.classList.add('hidden');
+  }
+}
+
+function handleClickedButton(event) {
+  var dataId = event.target.getAttribute('data-entry-id');
+
+  if (event.target.tagName !== ('data-entry-id')) {
+
   }
 }
