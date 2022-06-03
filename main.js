8b76dfb747d1047513152b9192ec97fd62c794db
Kevin Liang <kevinliang415@yahoo.com>
2022-06-03 22:21:06 +0000

    updated

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var modalButton = document.querySelector('.modal-btn');
 modalButton.addEventListener('click', handleClick);
 var form = document.querySelector('form');
-var spans = document.querySelectorAll('#week');
+var spans = document.querySelectorAll('span');
 spans.addEventListener('click', handleClickedButton);
 function handleClick(event) {
   if (event.target === modalButton) {
@@ -13,9 +13,16 @@ function handleClick(event) {
 }
 
 function handleClickedButton(event) {
-  var dataId = event.target.getAttribute('data-entry-id');
+  /* var dataId = event.target.getAttribute('data-entry-id'); */
 
   if (event.target.tagName !== ('data-entry-id')) {
-
+    for (var i = 0; i < spans.length; i++) {
+      spans[i].className = 'hidden';
+      if (spans[i] === event.target) {
+        spans[i].className = '';
+      } else {
+        spans[i].className = 'hidden';
+      }
+    }
   }
 }
