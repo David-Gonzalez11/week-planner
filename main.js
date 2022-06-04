13bb770a279e763a2511c353a097bb7be363aa42
Kevin Liang <kevinliang415@yahoo.com>
2022-06-04 00:10:28 +0000

    discussing how to populate chart with data

--- main.js
+++ main.js
@@ -17,7 +17,6 @@ function handleClickedButton(event) {
 
   if (event.target.tagName !== ('data-entry-id')) {
     for (var i = 0; i < spans.length; i++) {
-      spans[i].className = 'hidden';
       if (spans[i] === event.target) {
         spans[i].className = '';
       } else {
@@ -26,3 +25,45 @@ function handleClickedButton(event) {
     }
   }
 }
+/* var dummyData = [
+  {
+    time: '8: 00',
+    desc: 'Meeting with Jeff'
+  },
+  {
+    time: '10: 00',
+    desc: 'Take dog for a walk'
+  }
+];
+/* function createTable() {
+  var table = document.querySelector('table');
+  // Creates Table Head
+  var thead = document.createElement('thead');
+  var tr = document.createElement('tr');
+  var time = document.createElement('th');
+  time.textContent = 'Time';
+  var desc = document.createElement('th');
+  desc.textContent = 'Description';
+  tr.appendChild(time);
+  table.appendChild(thead).appendChild(tr).appendChild(desc);
+  // Creates Table Body
+  var tbody = document.createElement('tbody');
+  for (var i = 0; i < dummyData.length; i++) {
+    var innerTr = document.createElement('tr');
+    var timeTd = document.createElement('td');
+    var descTd = document.createElement('td');
+    timeTd.textContent = dummyData[i].time;
+    descTd.textContent = dummyData[i].desc;
+    innerTr.appendChild(timeTd);
+    tbody.appendChild(innerTr).appendChild(descTd);
+  }
+  table.appendChild(tbody);
+  return table;
+ } */
+
+// function renderEntriesToPage(event) {
+//   for (var i = 0; i < spans[i].length; i++) {
+//     createTable();
+//   }
+// }
+// console.log(createTable());
